5200c400514ef2826dabe5f6f5cf9533bba7696a
Jarum Carr <jarumlcarr@gmail.com>
2022-11-09 14:18:38 -0800

    getting the cart total to update bug fixing

--- shopping-cart.js
+++ shopping-cart.js
@@ -9,15 +9,17 @@ for (let i = 0; i < removeCartItemButton.length; i++) {
 
 function updateCartTotal() {
   let cartItemContainer = document.getElementsByClassName('cart-items')[0]
-  let cartRows = cartItemContainer.getElementsByClassName('cart-rows')
-  
-  for( let i = 0; i < cartRows; i++) {
+  let cartRows = cartItemContainer.getElementsByClassName('cart-row')
+  let total = 0
+  for( let i = 0; i < cartRows.length; i++) {
     let cartRows = cartRows[i]
     let priceElement = cartRow.getElementsByClassName('cart-price')[0]
     let quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
     let price = parseFloat(priceElement.innerText.replace('$', ''))
     let quantity = quantityElement.value
+    total = total + (price * quantity)
   }
+  document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total
 }
 
 
