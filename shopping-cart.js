5fcfad10a2158e60aa8bf24307ec989527c045ee
Jarum Carr <jarumlcarr@gmail.com>
2022-11-09 15:09:03 -0800

    finally fixed fuction updateCartTotal

--- shopping-cart.js
+++ shopping-cart.js
@@ -1,9 +1,11 @@
 const removeCartItemButton = document.getElementsByClassName('btn-danger')
+console.log(removeCartItemButton)
 for (let i = 0; i < removeCartItemButton.length; i++) {
   let button = removeCartItemButton[i];
   button.addEventListener('click', function(event) {
     let buttonClick = event.target
     buttonClick.parentElement.parentElement.remove()
+    updateCartTotal();
   })
 }
 
@@ -11,15 +13,16 @@ function updateCartTotal() {
   let cartItemContainer = document.getElementsByClassName('cart-items')[0]
   let cartRows = cartItemContainer.getElementsByClassName('cart-row')
   let total = 0
-  for( let i = 0; i < cartRows.length; i++) {
-    let cartRows = cartRows[i]
+  for (let i = 0; i < cartRows.length; i++) {
+    let cartRow = cartRows[i]
     let priceElement = cartRow.getElementsByClassName('cart-price')[0]
     let quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
     let price = parseFloat(priceElement.innerText.replace('$', ''))
     let quantity = quantityElement.value
-    total = total + (price * quantity)
+    newTotal = total + (price * quantity)
+    console.log(price)
   }
-  document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total
+  document.getElementsByClassName('cart-total-price')[0].innerText = '$' + newTotal
 }
 
 
